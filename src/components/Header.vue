3a494ae175117a14b87dca55e869bd12ab772c9d
Kevin Rivera <kevinsantiagoriverarestrepo@gmail.com>
2022-09-13 16:43:43 -0500

    Actualización de componentes y creación

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <img
+            @click="reload" 
+            src="@/assets/billete.png" 
+            alt="Logo"
+        >
+        <h1><span>Money</span>Tracking</h1>
+        <span></span>
+    </header>
+</template>
+
+<script>
+    export default {
+        methods: {
+            reload() {
+                location.reload();
+            }
+        }
+    };
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100vw;
+}
+img,
+span {
+  width: 48px;
+}
+h1 {
+  font-size: 1.4rem;
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,22 @@
+<template>    
+  <div class="splashscreen">
+    <img src="@/assets/billete.png" alt="logo">
+    <h1><span>Money</span>Tracking</h1>
+  </div>
+</template>
+
+<style scoped>
+.splashscreen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+h1 {
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
